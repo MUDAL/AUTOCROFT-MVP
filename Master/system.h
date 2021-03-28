ca378333c724b01dab6a02f0bbf60ee60adc5cd7
Olaoluwa Raji <46250887+MUDAL@users.noreply.github.com>
2021-03-28 12:50:12 +0100

    Add files via upload

--- Master/system.h
+++ Master/system.h
@@ -3,7 +3,7 @@
 
 extern void System_Init(void);
 extern void System_Timer_DelayMs(uint32_t delayTime);
-extern void System_Alarm_Init(sysTimer_t* pSysTimer, uint32_t taskRepTime);
+extern void System_Alarm_Init(sysTimer_t* pSysTimer, uint32_t timerRepTime);
 extern bool System_Alarm_Ready(sysTimer_t* pSysTimer);
 extern void System_Watchdog_Start(void);
 extern void System_Watchdog_Refresh(void);
