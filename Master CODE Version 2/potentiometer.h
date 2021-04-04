fdb6fd941af57d0c5b99f67c8efb4307bf0ce963
Olaoluwa Raji <46250887+MUDAL@users.noreply.github.com>
2021-04-04 20:49:04 +0100

    Delete Master CODE Version 2 directory

--- Master CODE Version 2/potentiometer.h
+++
@@ -1,8 +0,0 @@
-#ifndef _POTENTIOMETER_H
-#define _POTENTIOMETER_H
-
-extern void Potentiometer_Init(void);
-extern uint16_t Potentiometer_Get_RawPosition(void);
-extern uint8_t Potentiometer_Get_PercentPosition(void);
-
-#endif //_POTENTIOMETER_H
