54f53337b3fccaf7b999c8c1560df46c0daf5b7a
Olaoluwa Raji <46250887+MUDAL@users.noreply.github.com>
2021-06-02 22:01:15 +0100

    Update FSM.h

--- Autocroft Master Updated/FSM.h
+++ Autocroft Master Updated/FSM.h
@@ -5,8 +5,6 @@ extern void FSM_Init(ButtonDataStructure* pButton,
 										 MasterMessageDataStructure* pMasterToNode,
 										 NodeMessageDataStructure* pNodeToMaster);
 
-extern void FSM_Execute(ButtonDataStructure* pButton,
-										    MasterMessageDataStructure* pMasterToNode,
-										    NodeMessageDataStructure* pNodeToMaster);
+extern void FSM_Execute(void);
 
 #endif //FSM_H
